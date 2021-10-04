73dd9fd99b7d2b3b927723c47c23d7ae89a16411
JWA van Golverdinge Schut <jaron.van.golverdinge-schut@diymaxeda.com>
2021-10-04 11:41:05 +0200

    Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <Users />
+    <HelloWorld />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Users from '@/components/Users.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
-    Users,
+    HelloWorld, Users,
   },
 });
 </script>
